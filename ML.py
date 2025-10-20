0ee9d143a8af75b0898bec6ee40cb1cd85c37623
Arthur Touati <134126573+NFXSTUDIO@users.noreply.github.com>
2025-10-20 13:32:12 +0200

    GRU SVM

--- ML.py
+++ ML.py
@@ -3,8 +3,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import GRU, Dense, Input, Dropout
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import GRU, Dense, Dropout
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
@@ -198,7 +197,7 @@ def gru_svm():
     plt.savefig('gru_svm_training_history.png')
     plt.show()
 
-    # Plot training accuracy depending on time step
+    # Plot training accuracy depending on a time step
     plt.figure(figsize=(10, 6))
 
     # Create a model to get intermediate outputs at each time step
